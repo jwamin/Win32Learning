15e19cbfe23dbc154816893a4864e6451a1dc69e
Joss Manger <jossferatu@gmail.com>
2024-07-30 08:27:02 -0400

    additional drawing code, some cleanup

--- WindowsProject2/Windows-Learning.cpp
+++ WindowsProject2/Windows-Learning.cpp
@@ -147,31 +147,52 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             PAINTSTRUCT ps;
             LPPAINTSTRUCT pps = &ps;
-
+            const COLORREF rgbRed = 0x000000FF;
+            const COLORREF yellow = RGB(242, 252, 150);
+            TCHAR buffer[5000];
             POINT point;
             RECT rect;
-
+            SIZE size;
             HDC hdc = BeginPaint(hWnd, pps);
             // TODO: Add any drawing code that uses hdc here...
             LPRECT ptr = &rect;
+            LPSIZE sizePtr = &size;
+            HBRUSH brush = CreateSolidBrush(rgbRed);
+            HBRUSH yellowBrush = CreateSolidBrush(yellow);
+            
             GetClientRect(hWnd, ptr);
 
+            size.cx = rect.bottom - rect.top;
+            size.cy = rect.right - rect.left;
+            BOOL isSquare = size.cx == size.cy;
+
+            if (isSquare) {
+                MoveToEx(hdc, 0, 0, NULL);
+                FillRect(hdc, ptr, brush);
+            }
+
             point = getCenter(ptr);
-            
+
+            if (!isSquare) {
+                Arc(hdc, point.x - 100, point.y - 100, point.x + 100, point.y + 100, point.x, point.y, point.x, point.y);
+            }
+            else {
+                SelectObject(hdc, yellowBrush);
+                Ellipse(hdc, point.x - 100, point.y - 100, point.x + 100, point.y + 100);
+            }
+
             MoveToEx(hdc, 0, 0, NULL);
             LineTo(hdc, rect.right, rect.bottom);
-
             MoveToEx(hdc, rect.right, 0, NULL);
             LineTo(hdc, 0, rect.bottom);
             MoveToEx(hdc, point.x, point.y, NULL);
-            Arc(hdc, point.x - 100, point.y - 100, point.x + 100, point.y + 100, point.x, point.y, point.x, point.y);
 
             EndPaint(hWnd, pps);
 
-            INT height = rect.bottom - rect.top;
-            INT width = rect.right - rect.left;
-            TCHAR buffer[5000];
-            swprintf(buffer,256, L"hello width: %d, height: %d\n", width,height);
+            swprintf(buffer,256, L"hello width: %d, height: %d\n", size.cx,size.cy);
+            if (isSquare) {
+                swprintf(buffer, 256, L"it's square!\n");
+            }
             OutputDebugString(buffer);
 
         }
