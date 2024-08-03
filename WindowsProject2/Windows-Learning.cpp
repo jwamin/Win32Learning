c650bb576f0fa3971318661468f99bba654e6e55
Joss Manger <jossferatu@gmail.com>
2024-08-03 08:43:39 -0400

    busted window drawing code out to secondary source file

--- WindowsProject2/Windows-Learning.cpp
+++ WindowsProject2/Windows-Learning.cpp
@@ -3,7 +3,7 @@
 
 #include "framework.h"
 #include "Windows-Learning.h"
-#include "Helpers.h"
+#include "Drawing.h"
 
 #define MAX_LOADSTRING 100
 
@@ -145,56 +145,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     case WM_PAINT:
         {
-            PAINTSTRUCT ps;
-            LPPAINTSTRUCT pps = &ps;
-            const COLORREF rgbRed = 0x000000FF;
-            const COLORREF yellow = RGB(242, 252, 150);
-            TCHAR buffer[5000];
-            POINT point;
-            RECT rect;
-            SIZE size;
-            HDC hdc = BeginPaint(hWnd, pps);
-            // TODO: Add any drawing code that uses hdc here...
-            LPRECT ptr = &rect;
-            LPSIZE sizePtr = &size;
-            HBRUSH brush = CreateSolidBrush(rgbRed);
-            HBRUSH yellowBrush = CreateSolidBrush(yellow);
-            
-            GetClientRect(hWnd, ptr);
-
-            size.cx = rect.bottom - rect.top;
-            size.cy = rect.right - rect.left;
-            BOOL isSquare = size.cx == size.cy;
-
-            if (isSquare) {
-                MoveToEx(hdc, 0, 0, NULL);
-                FillRect(hdc, ptr, brush);
-            }
-
-            point = getCenter(ptr);
-
-            if (!isSquare) {
-                Arc(hdc, point.x - 100, point.y - 100, point.x + 100, point.y + 100, point.x, point.y, point.x, point.y);
-            }
-            else {
-                SelectObject(hdc, yellowBrush);
-                Ellipse(hdc, point.x - 100, point.y - 100, point.x + 100, point.y + 100);
-            }
-
-            MoveToEx(hdc, 0, 0, NULL);
-            LineTo(hdc, rect.right, rect.bottom);
-            MoveToEx(hdc, rect.right, 0, NULL);
-            LineTo(hdc, 0, rect.bottom);
-            MoveToEx(hdc, point.x, point.y, NULL);
-
-            EndPaint(hWnd, pps);
-
-            swprintf(buffer,256, L"hello width: %d, height: %d\n", size.cx,size.cy);
-            if (isSquare) {
-                swprintf(buffer, 256, L"it's square!\n");
-            }
-            OutputDebugString(buffer);
-
+        draw(hWnd);
         }
         break;
     case WM_DESTROY:
